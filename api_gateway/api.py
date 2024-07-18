0c650a7b6bf6657f70c2926ec362ab7e71d6b687
Piotr Wach <wach.piotrek@gmail.com>
2024-07-19 00:26:08 +0100

    api implemented

--- api_gateway/api.py
+++ api_gateway/api.py
@@ -1,7 +1,9 @@
 import functools
+import sqlite3
+from dataclasses import dataclass
 
 from flask import (
-    Blueprint, flash, g, redirect, render_template, request, session, url_for
+    Blueprint, current_app, flash, g, redirect, render_template, request, session, url_for, jsonify
 )
 from werkzeug.security import check_password_hash, generate_password_hash
 
@@ -9,26 +11,121 @@ from .db import get_db
 
 bp = Blueprint('auth', __name__, url_prefix='/')
 
-@bp.route("/room/join", methods=['POST'])
-def join_room():
-    return "<p>Hello, World!</p>"
 
-@bp.route("/room/leave", methods=['POST'])
-def leave_room():
-    return "<p>Hello, World!</p>"
+@dataclass
+class User:
+    id: int
+    name: str
 
-@bp.route("/rooms", methods=['GET'])
+
+@bp.route("/user/create", methods=['POST'])
+def create_user():
+    name = request.get_json()["name"]
+
+    try:
+        db = get_db()
+        cursor = db.cursor()
+        cursor.execute('INSERT INTO user (name) VALUES (?)', (name,))
+        db.commit()
+
+        return {"id": cursor.lastrowid}
+    except sqlite3.IntegrityError as ex:
+        if ex.sqlite_errorname == 'SQLITE_CONSTRAINT_UNIQUE':
+            return jsonify({'error': 'user already exists'}), 400
+        raise
+
+
+@bp.route("/user", methods=['GET'])
+def users():
+    db = get_db()
+    result = db.execute('SELECT * FROM user').fetchall()
+    return jsonify([ dict(**row) for row in result])
+
+
+@bp.route("/room/create", methods=['POST'])
+def create_room():
+    name = request.get_json()["name"]
+    try:
+        db = get_db()
+        cursor = db.cursor()
+        cursor.execute('INSERT INTO room (name) VALUES (?)', (name,))
+        db.commit()
+
+        return {"id": cursor.lastrowid}
+    except sqlite3.IntegrityError as ex:
+        if ex.sqlite_errorname == 'SQLITE_CONSTRAINT_UNIQUE':
+            return jsonify({'error': 'room already exists'}), 400
+        raise
+
+
+@bp.route("/room/<int:room_id>/join/<int:user_id>", methods=['POST'])
+def join_room(room_id, user_id):
+    try:
+        db = get_db()
+        cursor = db.cursor()
+        cursor.execute('INSERT INTO participant (room_id, user_id) VALUES (?, ?)', (room_id, user_id))
+        db.commit()
+
+        return {}
+    except sqlite3.IntegrityError as ex:
+        current_app.logger.exception("An unexpected error occurred")
+        return jsonify({'error': 'an error occurred while processing your request.'}), 500
+
+
+@bp.route("/room/<int:room_id>/leave/<int:user_id>", methods=['POST'])
+def leave_room(room_id, user_id):
+    try:
+        db = get_db()
+        cursor = db.cursor()
+        cursor.execute('DELETE FROM participant WHERE room_id = ? AND user_id = ?', (room_id, user_id))
+        db.commit()
+
+        return {}
+    except sqlite3.IntegrityError as ex:
+        current_app.logger.exception("An unexpected error occurred")
+        return jsonify({'error': 'an error occurred while processing your request.'}), 500
+
+
+@bp.route("/room", methods=['GET'])
 def rooms():
-    return "<p>Hello, World!</p>"
+    db = get_db()
+    result = db.execute('SELECT * FROM room').fetchall()
+    return jsonify([ dict(**row) for row in result])
 
-@bp.route("/room/<id>/participants", methods=['GET'])
-def room_participants():
-    return "<p>Hello, World!</p>"
 
-@bp.route("/room/<id>/messages", methods=['GET'])
-def get_messages():
-    return "<p>Hello, World!</p>"
+@bp.route("/room/<int:room_id>/participants", methods=['GET'])
+def room_participants(room_id):
+    db = get_db()
+    result = db.execute(
+        'SELECT user.id, user.name FROM user ' +
+        'JOIN participant ON user.id = participant.user_id ' + 
+        'JOIN room ON participant.room_id = room.id ' + 
+        'WHERE room.id = ?;', (room_id,)).fetchall()
+    return jsonify([ dict(**row) for row in result])
 
-@bp.route("/room/<id>/message", methods=['POST'])
-def send_message():
-    return "<p>Hello, World!</p>"
+
+@bp.route("/room/<int:room_id>/message", methods=['GET'])
+def get_messages(room_id):
+    db = get_db()
+    result = db.execute(
+        'SELECT message.id, message.created_at, message.text, user.name AS author_name ' +
+        'FROM message ' +
+        'JOIN user ON message.author_id = user.id ' +
+        'WHERE message.room_id = ?;', (room_id,)).fetchall()
+    return jsonify([ dict(**row) for row in result])
+
+
+@bp.route("/room/<int:room_id>/user/<int:author_id>/message", methods=['POST'])
+def send_message(room_id, author_id):
+    text = request.get_json()["text"]
+
+    try:
+        db = get_db()
+        cursor = db.cursor()
+        cursor.execute('INSERT INTO message (room_id, author_id, text) VALUES (?, ?, ?)', (room_id, author_id, text))
+        db.commit()
+
+        return {}
+    except sqlite3.IntegrityError as ex:
+        current_app.logger.exception("An unexpected error occurred")
+        return jsonify({'error': 'an error occurred while processing your request.'}), 500
