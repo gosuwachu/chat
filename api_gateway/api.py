3b7e18caf7db63b0cefc67e0c3c6bd36a3a4e80d
Piotr Wach <pwach@bloomberg.net>
2024-07-19 22:27:20 +0100

    redis in docker and redis message queue

--- api_gateway/api.py
+++ api_gateway/api.py
@@ -1,6 +1,12 @@
 from .db import get_db
 
+import redis
+import json
+from datetime import datetime
+
 SYSTEM_USER_ID=1
+
+redis_client = redis.StrictRedis(host='localhost', port=6379, db=0)
 
 def get_user_name(user_id):
     db = get_db()
@@ -20,3 +26,27 @@ def send_message(room_id, author_id, text):
     cursor = db.cursor()
     cursor.execute('INSERT INTO message (room_id, author_id, text) VALUES (?, ?, ?)', (room_id, author_id, text))
     db.commit()
+
+    message_id = cursor.lastrowid
+    cursor.execute('SELECT created_at FROM message WHERE id = ?', (message_id,))
+    created_at: datetime = cursor.fetchone()[0]
+
+    # Create message payload
+    message_payload = {
+        'message': {
+            'room_id': room_id,
+            'author_id': author_id,
+            'text': text,
+            'created_at': created_at.isoformat()
+        }
+    }
+    
+    # Get participants of the room
+    cursor.execute('SELECT user_id FROM participant WHERE room_id = ?', (room_id,))
+    participants = cursor.fetchall()
+    
+    # Enqueue message in Redis for each participant
+    for participant in participants:
+        participant_id = participant[0]
+        redis_client.rpush(f'user:{participant_id}', json.dumps(message_payload))
+    
