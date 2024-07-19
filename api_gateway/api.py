819b340240ceda4d17abbe92e8cce4964688dddd
Piotr Wach <pwach@bloomberg.net>
2024-07-19 22:33:09 +0100

    better redis api

--- api_gateway/api.py
+++ api_gateway/api.py
@@ -21,6 +21,12 @@ def get_user_name(user_id):
 def send_system_message(room_id, text):
     send_message(room_id, SYSTEM_USER_ID, text)
 
+def get_participants(room_id):
+    db = get_db()
+    cursor = db.cursor()
+    cursor.execute('SELECT user_id FROM participant WHERE room_id = ?', (room_id,))
+    return [p[0] for p in cursor.fetchall()]
+
 def send_message(room_id, author_id, text):
     db = get_db()
     cursor = db.cursor()
@@ -31,22 +37,19 @@ def send_message(room_id, author_id, text):
     cursor.execute('SELECT created_at FROM message WHERE id = ?', (message_id,))
     created_at: datetime = cursor.fetchone()[0]
 
-    # Create message payload
-    message_payload = {
+    enqueue_for_participants(room_id, {
         'message': {
             'room_id': room_id,
             'author_id': author_id,
             'text': text,
             'created_at': created_at.isoformat()
         }
-    }
+    })
     
-    # Get participants of the room
-    cursor.execute('SELECT user_id FROM participant WHERE room_id = ?', (room_id,))
-    participants = cursor.fetchall()
-    
-    # Enqueue message in Redis for each participant
-    for participant in participants:
-        participant_id = participant[0]
-        redis_client.rpush(f'user:{participant_id}', json.dumps(message_payload))
-    
+def enqueue_for_participants(room_id, event):
+    participants = get_participants(room_id)
+    for participant_id in participants:
+        enqueue_event(participant_id, json.dumps(event))
+
+def enqueue_event(user_id, event):
+    redis_client.rpush(f'user:{user_id}', json.dumps(event))
